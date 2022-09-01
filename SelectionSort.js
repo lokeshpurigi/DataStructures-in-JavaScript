d60a1620e19452bcd302166a79a85edf31b2c168
lokesh purigi <plokesh7428@gmail.com>
2022-09-01 10:42:31 +0530

    Selection- Sort in dataStruc

---
+++ SelectionSort.js
@@ -0,0 +1,22 @@
+function Swap(arr, xp, yp) {
+  var temp = arr[xp];
+  arr[xp] = arr[yp];
+  arr[yp] = temp;
+}
+function SelectionSort(arr, n) {
+  var i, j, min_idx;
+  for (i = 0; i < n - 1; i++) {
+    min_idx = i;
+    for (j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min_idx]) {
+        min_idx = j;
+      }
+    }
+    Swap(arr, min_idx, i);
+  }
+  console.log(arr);
+}
+
+var arr = [65, 56, 78, 39, 11];
+var n = 5;
+SelectionSort(arr, n);
