73a4ebe5a7e604517caad1f9de7731419014d634
lokesh purigi <plokesh7428@gmail.com>
2022-09-01 14:57:49 +0530

    bubblee

--- Bubble-sort.js
+++ Bubble-sort.js
@@ -1,8 +1,11 @@
 function bblSort(arr){
     for(var i = 0 ; i < arr.length; i++ ){
-        for(var j = 0 ; j < arr.length-i-1; j++)
+        for(var j = 0 ; j < arr.length-1-i; j++)
         {
-            if(arr[j] > arr[j+1])
+            if(arr[j] > arr[j+1])  //{
+
+            // [arr[j],arr[j+1]] = [arr[j+1],arr[j]]  // gives descending order 
+            // }
             {
                 var temp =arr[j]
                 arr[j] = arr[j+1]
@@ -14,3 +17,9 @@ function bblSort(arr){
 } 
 var arr = [34,3,56,7,8];
 bblSort(arr);
+
+// 3,34,56,7,8
+
+// 3,34,7,56,8
+
+// 3,7,34,8 ,56
